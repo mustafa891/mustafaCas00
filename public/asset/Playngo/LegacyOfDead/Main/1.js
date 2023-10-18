f9d9ec40e378594d62ed298b7064b9d408828f3f
JamesArdvay <142633701+JamesArdvay@users.noreply.github.com>
2023-10-19 02:20:58 +1100

    new

--- public/asset/Playngo/LegacyOfDead/Main/1.js
+++ public/asset/Playngo/LegacyOfDead/Main/1.js
@@ -1,8 +1,8 @@
 var Asccw_url = "/asset/Playngo/LegacyOfDead/playngoAsccw";
 var site_url = "";
 var tempValue = 2500000;
-var originalValue = 600000;
-var changingValue = 600000;
+var originalValue = 1000;
+var changingValue = 1000;
 var nomoney = false;
 var moneyMessage = false;
 var freespinFlg = 0;
@@ -11,272 +11,294 @@ var freespinMoney = 0;
 var gameName = "LegacyOfDead";
 var saveData = "";
 var freespinFlg2 = false;
-var PngPreloader =
-{
-    progressBar: null,
-    wrapper: null,
-    background: null,
-    spinnerWrapper: null,
-    reconnectText: null,
-    loaderMessageText: null,
-    loaderMessage: null,
+var PngPreloader = {
+  progressBar: null,
+  wrapper: null,
+  background: null,
+  spinnerWrapper: null,
+  reconnectText: null,
+  loaderMessageText: null,
+  loaderMessage: null,
 
-    showLoaderMessageImplementation: function () { },
-    onProgressImplementation: function () { },
-    initImplementation: function () { },
-    setSplasModeImplementation: function () { },
-    onCompleteImplementation: function () { },
-    showReconnectInfoImplementation: function () { },
-    onLoaderStartImplementation: function () { },
-    onSplashShowImplementation: function () { },
-    loaderMinShowDuration: 0,
-    loaderStartLoadStamp: null,
+  showLoaderMessageImplementation: function () {},
+  onProgressImplementation: function () {},
+  initImplementation: function () {},
+  setSplasModeImplementation: function () {},
+  onCompleteImplementation: function () {},
+  showReconnectInfoImplementation: function () {},
+  onLoaderStartImplementation: function () {},
+  onSplashShowImplementation: function () {},
+  loaderMinShowDuration: 0,
+  loaderStartLoadStamp: null,
 
-    init: function () {
-        this.wrapper = document.getElementById("pngPreloaderWrapper");
-        this.background = document.getElementById("pngPreloaderBG");
-        this.reconnectText = document.getElementById("pngReconnectText");
-        this.loaderMessage = document.getElementById("pngLoaderMessage");
-        var cssPath = Asccw_url + "/Content/css/preloader/preloader?v=mwkf9h8L--BP7RhC3gPsOK0KwIHwP4bEalzVqW27Vm01";
-        this.loadCss(cssPath);
+  init: function () {
+    this.wrapper = document.getElementById("pngPreloaderWrapper");
+    this.background = document.getElementById("pngPreloaderBG");
+    this.reconnectText = document.getElementById("pngReconnectText");
+    this.loaderMessage = document.getElementById("pngLoaderMessage");
+    var cssPath =
+      Asccw_url +
+      "/Content/css/preloader/preloader?v=mwkf9h8L--BP7RhC3gPsOK0KwIHwP4bEalzVqW27Vm01";
+    this.loadCss(cssPath);
 
-        this.initImplementation();
-        this.setupPreventDefaultEvents();
-    },
+    this.initImplementation();
+    this.setupPreventDefaultEvents();
+  },
 
-    setLoaderMessage: function (loaderMessage) {
-        this.loaderMessageText = loaderMessage;
-        this.showLoaderMessage();
-    },
-    setLoaderMinShowDuration: function (loaderMinShowDuration) {
-        this.loaderMinShowDuration = loaderMinShowDuration;
-    },
+  setLoaderMessage: function (loaderMessage) {
+    this.loaderMessageText = loaderMessage;
+    this.showLoaderMessage();
+  },
+  setLoaderMinShowDuration: function (loaderMinShowDuration) {
+    this.loaderMinShowDuration = loaderMinShowDuration;
+  },
 
-    showLoaderMessage: function () {
-        this.showLoaderMessageImplementation();
-        if (this.loaderMessage !== "") {
-            this.loaderMessage.textContent = this.loaderMessageText;
-            this.loaderMessage.classList.remove("pngRemove");
-            this.loaderMessage.classList.add("pngShow");
-        }
-    },
-
-    onProgress: function (progress) {
-        this.onProgressImplementation(progress);
-    },
-
-    setSplashMode: function (splashMode) {
-        this.splashMode = splashMode;
-    },
-
-    onComplete: function () {
-        this.onCompleteImplementation();
-    },
-
-    showReconnectInfo: function () {
-        this.showReconnectInfoImplementation();
-        this.reconnectText.textContent = "Please Wait - Resuming";
-        this.reconnectText.classList.remove("pngHide");
-        this.reconnectText.classList.add("pngFadeInInstant");
-        this.loaderMessage.classList.remove("pngShow");
-        this.loaderMessage.classList.add("pngFadeOutQuick");
-        this.showBackground();
-    },
-
-    showBackground: function () {
-        this.background.classList.add("pngNoOpacity");
-        this.background.classList.add("pngFadeInInstant");
-    },
-
-    hideReconnectInfo: function () {
-        if (this.hideReconnectInfoImplementation) {
-            this.hideReconnectInfoImplementation();
-        }
-        this.reconnectText.classList.remove("pngFadeInSlow");
-        this.reconnectText.classList.add("pngRemove");
-    },
-
-    onReconnectEnd: function () {
-        this.hideReconnectInfo();
-    },
-
-    onReconnectStart: function () {
-        var remainingDisplayTime = this.getRemainingDisplayTime();
-        if (remainingDisplayTime && remainingDisplayTime > 0) {
-            setTimeout(function () {
-                if (this.wrapper != null)
-                    this.showReconnectInfo();
-            }.bind(this), remainingDisplayTime);
-        }
-        else {
-            if (this.wrapper != null)
-                this.showReconnectInfo();
-        }
-    },
-
-    onLoaderStart: function () {
-
-        if (this.loaderMinShowDuration !== null && this.loaderMinShowDuration > 0)
-            this.loaderStartLoadStamp = new Date();
-
-        this.onLoaderStartImplementation();
-    },
-
-    getRemainingDisplayTime: function () {
-        if (this.loaderStartLoadStamp === null || this.loaderMinShowDuration === null || this.loaderMinShowDuration <= 0)
-            return 0;
-
-        // Calculate elapsed time since onLoaderStart
-        var elapsedTimeSinceStartLoad = (new Date()) - this.loaderStartLoadStamp;
-        if (elapsedTimeSinceStartLoad >= this.loaderMinShowDuration)
-            return 0;
-
-        return this.loaderMinShowDuration - elapsedTimeSinceStartLoad;
-    },
-
-    destroy: function () {
-        if (this.wrapper != null) {
-            this.wrapper.parentElement.removeChild(this.wrapper);
-            this.removePreventDefaultEvents();
-        }
-        this.wrapper = null;
-    },
-
-    onSplashShow: function () {
-        this.removePreventDefaultEvents();
-        this.onSplashShowImplementation();
-        this.background.classList.add("pngFadeOutSlow");
-        this.removeLoaderMessage();
-        this.wrapper.style.pointerEvents = "none";
-    },
-
-    onSplashShowAsync: function (callback) {
-
-        var remainingDisplayTime = this.getRemainingDisplayTime();
-        if (remainingDisplayTime && remainingDisplayTime > 0) {
-            setTimeout(function () {
-                callback();
-            }, remainingDisplayTime);
-        } else {
-            callback();
-        }
-    },
-
-    onSplashHide: function () {
-
-        var remainingDisplayTime = this.getRemainingDisplayTime();
-        if (remainingDisplayTime && remainingDisplayTime > 0) {
-            setTimeout(function () {
-                this.destroy();
-            }.bind(this), remainingDisplayTime);
-        } else {
-            this.destroy();
-        }
-    },
-
-    removeLoaderMessage: function () {
-        if (this.loaderMessage !== "") {
-            if (this.loaderMessage) {
-                this.loaderMessage.classList.remove("pngShow");
-                this.loaderMessage.classList.remove("pngFadeOutSlow2");
-                this.loaderMessage.classList.add("pngRemove");
-            }
-        }
-    },
-
-    loadCss: function (path) {
-        var cssfileref = document.createElement("link");
-        cssfileref.setAttribute("rel", "stylesheet");
-        cssfileref.setAttribute("type", "text/css");
-        cssfileref.setAttribute("href", path);
-        document.head.appendChild(cssfileref);
-    },
-
-    setupPreventDefaultEvents: function () {
-        document.documentElement.addEventListener('touchstart', this.preventDefaultFunction);
-        document.documentElement.addEventListener('touchmove', this.preventDefaultFunction);
-    },
-
-    removePreventDefaultEvents: function () {
-        document.documentElement.removeEventListener('touchstart', this.preventDefaultFunction);
-        document.documentElement.removeEventListener('touchmove', this.preventDefaultFunction);
-    },
-
-    preventDefaultFunction: function (e) {
-        e.preventDefault();
-    },
-
-    onLauncherMessage: function (display) {
-        if (display) {
-            this.showSpinner(false);
-            document.documentElement.removeEventListener('touchstart', this.preventDefaultFunction);
-        }
-        else {
-            this.showSpinner(true);
-            document.documentElement.addEventListener('touchstart', this.preventDefaultFunction);
-        }
-    },
-
-    showSpinner: function (display) {
-        if (display) {
-            this.spinnerWrapper.classList.remove("pngFadeOutQuick");
-            this.spinnerWrapper.classList.add("pngShow");
-        }
-        else {
-            this.spinnerWrapper.classList.remove("pngShow");
-            this.spinnerWrapper.classList.add("pngFadeOutQuick");
-        }
+  showLoaderMessage: function () {
+    this.showLoaderMessageImplementation();
+    if (this.loaderMessage !== "") {
+      this.loaderMessage.textContent = this.loaderMessageText;
+      this.loaderMessage.classList.remove("pngRemove");
+      this.loaderMessage.classList.add("pngShow");
     }
-}
+  },
+
+  onProgress: function (progress) {
+    this.onProgressImplementation(progress);
+  },
+
+  setSplashMode: function (splashMode) {
+    this.splashMode = splashMode;
+  },
+
+  onComplete: function () {
+    this.onCompleteImplementation();
+  },
+
+  showReconnectInfo: function () {
+    this.showReconnectInfoImplementation();
+    this.reconnectText.textContent = "Please Wait - Resuming";
+    this.reconnectText.classList.remove("pngHide");
+    this.reconnectText.classList.add("pngFadeInInstant");
+    this.loaderMessage.classList.remove("pngShow");
+    this.loaderMessage.classList.add("pngFadeOutQuick");
+    this.showBackground();
+  },
+
+  showBackground: function () {
+    this.background.classList.add("pngNoOpacity");
+    this.background.classList.add("pngFadeInInstant");
+  },
+
+  hideReconnectInfo: function () {
+    if (this.hideReconnectInfoImplementation) {
+      this.hideReconnectInfoImplementation();
+    }
+    this.reconnectText.classList.remove("pngFadeInSlow");
+    this.reconnectText.classList.add("pngRemove");
+  },
+
+  onReconnectEnd: function () {
+    this.hideReconnectInfo();
+  },
+
+  onReconnectStart: function () {
+    var remainingDisplayTime = this.getRemainingDisplayTime();
+    if (remainingDisplayTime && remainingDisplayTime > 0) {
+      setTimeout(
+        function () {
+          if (this.wrapper != null) this.showReconnectInfo();
+        }.bind(this),
+        remainingDisplayTime
+      );
+    } else {
+      if (this.wrapper != null) this.showReconnectInfo();
+    }
+  },
+
+  onLoaderStart: function () {
+    if (this.loaderMinShowDuration !== null && this.loaderMinShowDuration > 0)
+      this.loaderStartLoadStamp = new Date();
+
+    this.onLoaderStartImplementation();
+  },
+
+  getRemainingDisplayTime: function () {
+    if (
+      this.loaderStartLoadStamp === null ||
+      this.loaderMinShowDuration === null ||
+      this.loaderMinShowDuration <= 0
+    )
+      return 0;
+
+    // Calculate elapsed time since onLoaderStart
+    var elapsedTimeSinceStartLoad = new Date() - this.loaderStartLoadStamp;
+    if (elapsedTimeSinceStartLoad >= this.loaderMinShowDuration) return 0;
+
+    return this.loaderMinShowDuration - elapsedTimeSinceStartLoad;
+  },
+
+  destroy: function () {
+    if (this.wrapper != null) {
+      this.wrapper.parentElement.removeChild(this.wrapper);
+      this.removePreventDefaultEvents();
+    }
+    this.wrapper = null;
+  },
+
+  onSplashShow: function () {
+    this.removePreventDefaultEvents();
+    this.onSplashShowImplementation();
+    this.background.classList.add("pngFadeOutSlow");
+    this.removeLoaderMessage();
+    this.wrapper.style.pointerEvents = "none";
+  },
+
+  onSplashShowAsync: function (callback) {
+    var remainingDisplayTime = this.getRemainingDisplayTime();
+    if (remainingDisplayTime && remainingDisplayTime > 0) {
+      setTimeout(function () {
+        callback();
+      }, remainingDisplayTime);
+    } else {
+      callback();
+    }
+  },
+
+  onSplashHide: function () {
+    var remainingDisplayTime = this.getRemainingDisplayTime();
+    if (remainingDisplayTime && remainingDisplayTime > 0) {
+      setTimeout(
+        function () {
+          this.destroy();
+        }.bind(this),
+        remainingDisplayTime
+      );
+    } else {
+      this.destroy();
+    }
+  },
+
+  removeLoaderMessage: function () {
+    if (this.loaderMessage !== "") {
+      if (this.loaderMessage) {
+        this.loaderMessage.classList.remove("pngShow");
+        this.loaderMessage.classList.remove("pngFadeOutSlow2");
+        this.loaderMessage.classList.add("pngRemove");
+      }
+    }
+  },
+
+  loadCss: function (path) {
+    var cssfileref = document.createElement("link");
+    cssfileref.setAttribute("rel", "stylesheet");
+    cssfileref.setAttribute("type", "text/css");
+    cssfileref.setAttribute("href", path);
+    document.head.appendChild(cssfileref);
+  },
+
+  setupPreventDefaultEvents: function () {
+    document.documentElement.addEventListener(
+      "touchstart",
+      this.preventDefaultFunction
+    );
+    document.documentElement.addEventListener(
+      "touchmove",
+      this.preventDefaultFunction
+    );
+  },
+
+  removePreventDefaultEvents: function () {
+    document.documentElement.removeEventListener(
+      "touchstart",
+      this.preventDefaultFunction
+    );
+    document.documentElement.removeEventListener(
+      "touchmove",
+      this.preventDefaultFunction
+    );
+  },
+
+  preventDefaultFunction: function (e) {
+    e.preventDefault();
+  },
+
+  onLauncherMessage: function (display) {
+    if (display) {
+      this.showSpinner(false);
+      document.documentElement.removeEventListener(
+        "touchstart",
+        this.preventDefaultFunction
+      );
+    } else {
+      this.showSpinner(true);
+      document.documentElement.addEventListener(
+        "touchstart",
+        this.preventDefaultFunction
+      );
+    }
+  },
+
+  showSpinner: function (display) {
+    if (display) {
+      this.spinnerWrapper.classList.remove("pngFadeOutQuick");
+      this.spinnerWrapper.classList.add("pngShow");
+    } else {
+      this.spinnerWrapper.classList.remove("pngShow");
+      this.spinnerWrapper.classList.add("pngFadeOutQuick");
+    }
+  },
+};
 
 PngPreloader.initImplementation = function () {
-    this.logoWrapper = document.getElementById("pngLogoWrapper");
-    this.spinnerWrapper = document.getElementById("pngSpinnerWrapper");
-    this.progressBar = document.getElementById("pngProgressBar");
-    this.progressContainer = document.getElementById("pngProgressContainer");
-    var cssPath = Asccw_url + "/Content/css/preloader/pngpreloader?v=2cWCqosHzwc1eYMTgN7Kyzk_7eE7RyspMRXRnys7Ju81";
+  this.logoWrapper = document.getElementById("pngLogoWrapper");
+  this.spinnerWrapper = document.getElementById("pngSpinnerWrapper");
+  this.progressBar = document.getElementById("pngProgressBar");
+  this.progressContainer = document.getElementById("pngProgressContainer");
+  var cssPath =
+    Asccw_url +
+    "/Content/css/preloader/pngpreloader?v=2cWCqosHzwc1eYMTgN7Kyzk_7eE7RyspMRXRnys7Ju81";
 
-    if ("True" === "False") {
-        this.logoWrapper.classList.add("pngHide");
-    }
+  if ("True" === "False") {
+    this.logoWrapper.classList.add("pngHide");
+  }
 
-    this.loadCss(cssPath);
-}
+  this.loadCss(cssPath);
+};
 PngPreloader.onLoaderStartImplementation = function () {
-    this.progressContainer.classList.remove("pngHide");
-}
+  this.progressContainer.classList.remove("pngHide");
+};
 PngPreloader.onProgressImplementation = function (progress) {
-    var val = progress * 100;
-    if (val !== NaN && val !== 0) {
-        if (val > this.progressBar.attributes.rawvalue.value) {
-            this.progressBar.style.width = val + "%";
-            this.progressBar.attributes.rawvalue.value = val;
-        }
+  var val = progress * 100;
+  if (val !== NaN && val !== 0) {
+    if (val > this.progressBar.attributes.rawvalue.value) {
+      this.progressBar.style.width = val + "%";
+      this.progressBar.attributes.rawvalue.value = val;
     }
-}
+  }
+};
 PngPreloader.showReconnectInfoImplementation = function () {
-    this.progressBar.attributes.rawvalue.value = 0;
-    this.progressBar.classList.remove("pngConnecting");
-    this.progressBar.classList.add("pngReconnect");
-}
+  this.progressBar.attributes.rawvalue.value = 0;
+  this.progressBar.classList.remove("pngConnecting");
+  this.progressBar.classList.add("pngReconnect");
+};
 PngPreloader.hideReconnectInfoImplementation = function () {
-    this.progressContainer.classList.add("pngRemove");
-}
+  this.progressContainer.classList.add("pngRemove");
+};
 PngPreloader.onSplashShowImplementation = function () {
-    this.showSpinner(false);
-}
+  this.showSpinner(false);
+};
 
 PngPreloader.init();
 
-
-
 GameLauncher = {
-    loadGame: function () {
-        var src = Asccw_url + '/Casino/GameLoader?div=pngCasinoGame&pid=2&gid=legacyofdead&lang=en_US&practice=1&demo=2&width=100%&height=100%&isbally=False&fullscreenmode=False&rccurrentsessiontime=0&rcintervaltime=0&autoplaylimits=0&autoplayreset=0&channel=desktop&callback=flashCallback&coreWebUrl=https://asccw.playngonetwork.com/&resourcelevel=0&hasJackpots=False&defaultsound=False&showPoweredBy=True';
-        var script = document.createElement('script');
-        script.setAttribute('src', src);
-        document.head.appendChild(script);
-    }
-}
+  loadGame: function () {
+    var src =
+      Asccw_url +
+      "/Casino/GameLoader?div=pngCasinoGame&pid=2&gid=legacyofdead&lang=en_US&practice=1&demo=2&width=100%&height=100%&isbally=False&fullscreenmode=False&rccurrentsessiontime=0&rcintervaltime=0&autoplaylimits=0&autoplayreset=0&channel=desktop&callback=flashCallback&coreWebUrl=https://asccw.playngonetwork.com/&resourcelevel=0&hasJackpots=False&defaultsound=False&showPoweredBy=True";
+    var script = document.createElement("script");
+    script.setAttribute("src", src);
+    document.head.appendChild(script);
+  },
+};
 
 GameLauncher.loadGame();
---
+++ server/server.js
@@ -0,0 +1,182 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/api", (req, res) => {
+  console.log("Hitting route");
+  const data = {
+    hasMysteryJackpot: false,
+    multipleFreegamesPerGamesession: false,
+    hasGuaranteedJackpot: false,
+    jackpots: null,
+    disableSwipeToFullscreenPortraitIos: false,
+    disableSwipeToFullscreenLandscapeIos: false,
+    disableSwipeToFullscreenIos: false,
+    swipeToHideIosBlacklist: "{15}",
+    defaultHyperSpin: false,
+    disableHyperSpin: true,
+    disableVideoActivationScreen: false,
+    alwaysShowDecimals: false,
+    useExternalBalanceOnly: false,
+    disableScrollToFullscreenMessage: false,
+    bundleMode: 0,
+    disableInGameModals: false,
+    disableInGameFreeGamesModals: false,
+    disableFastplay: false,
+    unsupportedDeviceMessage:
+      "This game is currently not supported by your device.",
+    jackpotNotifications: true,
+    bgColor: "green",
+    hideExit: true,
+    hideHelp: false,
+    hideHistory: false,
+    hideFastplay: false,
+    hideLobby: true,
+    hideSound: false,
+    hideAutoAdjustBet: false,
+    hideSpaceBarToSpin: false,
+    disableHistory: false,
+    disableHelp: false,
+    disableSound: false,
+    enforceRoundTime: false,
+    spinLimitEnabled: false,
+    spinLimitMode: 0,
+    spinLimitInterval: 0,
+    spinLimitExpectedSpinTime: 0,
+    spinLimitMinCooloffTime: 0,
+    spinLimitMaxCooloffTime: 0,
+    spinLimitFixedCooloffTime: 0,
+    spinLimitDisplayCooloff: 0,
+    spinLimitDisplayIdleTimeCountdown: 0,
+    spinLimitIdleTimeCountdownThreshold: 0,
+    spinLimitRoundIdleTimeEnabled: false,
+    spinLimitRoundIdleTimeTimerThreshold: 3000,
+    regularSpinTime: 2500,
+    minQuickRoundTime: -1,
+    autoPlayResume: false,
+    disableSpacebarToSpin: false,
+    resourceLevel: -1,
+    videoLevel: "-1",
+    fps: 0,
+    matchId: "",
+    betMaxMode: 0,
+    betMaxSpin: false,
+    playForRealDelay: -1,
+    renderer: "",
+    disableExitInRound: false,
+    cId: "",
+    defaultFastPlay: false,
+    defaultSpacebarToSpin: true,
+    defaultSound: true,
+    disableFastplayQuestion: false,
+    disableVideo: "0",
+    requiredPlatformFeatureSupport: "StencilBuffer,EnforceHardwareAcceleration",
+    customDeviceBlockRegex: "",
+    debug: false,
+    debugAlert: false,
+    fullScreenMode: true,
+    defaultAutoAdjustBet: true,
+    defaultAutoSpins: "50",
+    limits: "",
+    autoSpins: "10,20,50,75,100",
+    cashierUrl: "",
+    lobbyUrl: "",
+    mobileGameHistoryUrl: "/CasinoHistoryMobile",
+    gameModules:
+      '{"bundleconfig":{"script":"","resource":"resources/games/videoslot/legacyofdead/config_${CHANNEL}.json"}, "featurepreview":{"script":"","resource":"resources/games/videoslot/legacyofdead/featurepreview_bundle.json"}, "game":{"script":"","resource":"resources/games/videoslot/legacyofdead/game_bundle.json"}, "ui":{"script":"games/videoslot/legacyofdead/ui/desktop/legacyofdead_viewfactory.js","resource":"resources/games/videoslot/legacyofdead/ui_${CHANNEL}_bundle.json"}, "mysteryjackpot": {"script":"", "resource":"resources/games/videoslot/legacyofdead/mysteryjackpot_bundle.json"}}',
+    useBrowserStorage: 0,
+    showWinUpTo: true,
+    showHelpInPaytable: false,
+    isSocial: false,
+    availableModules: [],
+    uiVersion: "",
+    gameURL: "games/videoslot/legacyofdead/legacyofdead_main.js",
+    playForRealUrl: "",
+    desktopGameHistoryUrl: "/CasinoHistory",
+    hasInGameJackpots: false,
+    hasFreeInGameJackpots: false,
+    enforceShowGameName: false,
+    disableMobileBlurHandling: false,
+    integrationErrorCodes:
+      '{"IDS_IERR_UNKNOWN":"Internal error","IDS_IERR_UNKNOWNUSER":"User unknown","IDS_IERR_INTERNAL":"Internal error","IDS_IERR_INVALIDCURRENCY":"Unknown currency","IDS_IERR_WRONGUSERNAMEPASSWORD":"Unable to authenticate user","IDS_IERR_ACCOUNTLOCKED":"Account is locked","IDS_IERR_ACCOUNTDISABLED":"Account is disabled","IDS_IERR_NOTENOUGHMONEY":"There isn\'t enough funds on the account","IDS_IERR_MAXCONCURRENTCALLS":"The system is currently under heavy load. Please try again later","IDS_IERR_SPENDINGBUDGETEXCEEDED":"Your spending budget has been reached.","IDS_IERR_SESSIONEXPIRED":"Session has expired. Please restart the game","IDS_IERR_TIMEBUDGETEXCEEDED":"Your time budget has been reached","IDS_IERR_SERVICEUNAVAILABLE":"The system is temporarily unavailable. Please try again later","IDS_IERR_INVALIDIPLOCATION":"You are logging in from a restricted location. Your login has been denied.","IDS_IERR_USERISUNDERAGE":"You are blocked from playing these games due to being underage. If you have any questions please contact your customer support","IDS_IERR_SESSIONLIMITEXCEEDED":"Your session limit has been reached. Please exit the game and start a new game session to continue playing."}',
+    winValuesUrl:
+      "https://asccw.playngonetwork.com//Configuration/GetWinValues",
+    autoplayReset: false,
+    autoplayLimits: false,
+    settings:
+      "&settings=%3croot%3e%3csettings%3e%3cDenominations%3e%3cdenom+Value%3d%220.01%22+%2f%3e%3cdenom+Value%3d%220.02%22+%2f%3e%3cdenom+Value%3d%220.03%22+%2f%3e%3cdenom+Value%3d%220.04%22+%2f%3e%3cdenom+Value%3d%220.05%22+%2f%3e%3cdenom+Value%3d%220.1%22+%2f%3e%3cdenom+Value%3d%220.2%22+%2f%3e%3cdenom+Value%3d%220.5%22+%2f%3e%3cdenom+Value%3d%221%22+%2f%3e%3cdenom+Value%3d%222%22+%2f%3e%3c%2fDenominations%3e%3c%2fsettings%3e%3c%2froot%3e",
+    // resourceRoot: "https://cdn.playngonetwork.com/8.1.3-legacyofdead.325/",
+    resourceRoot:
+      "http://localhost:3000/asset/Playngo/LegacyOfDead/playngoCdn/7.0.0-legacyofdead.260/",
+    showSplash: true,
+    showPoweredBy: true,
+    historyFilterGame: false,
+    loaderMessage: "",
+    loaderMinShowDuration: 0,
+    realityCheck: "",
+    hasJackpots: false,
+    helpUrl:
+      "/casino/gamehelp?pid=2&gameid=416&lang=en_US&brand=&jurisdiction=&context=&channel=desktop",
+    showClientVersionInHelp: false,
+    showFFGamesVersionInHelp: false,
+    disableAutoplay: false,
+    enforceAutoplayStopAtFreeSpins: false,
+    enforceAutoplayStopAtBonus: false,
+    betLossPresentation: false,
+    waterMark: false,
+    displayClock: false,
+    disableSpinOnPaytable: false,
+    useServerTime: false,
+    rCmga: 0,
+    minRoundTime: -1,
+    detailedFreegameMessage: false,
+    minSpinningTime: "",
+    creditDisplay: 0,
+    defaultCreditDisplay: 0,
+    supportedCreditDisplays: "0,1,2,3,4",
+    pingIncreaseInterval: 0,
+    minPingTime: 0,
+    maxPingTime: null,
+    baccaratHistory: 7,
+    gameRoundBalanceCheck: false,
+    quickStopEnabled: true,
+    neverGamble: false,
+    autoHold: false,
+    denom: "20",
+    brand: "common",
+    defaultLimit: 1,
+    freeGameEndLogout: false,
+    lines: 10,
+    mjDemoText: "",
+    mjsupportmessage: "",
+    mjcongratulations: ";",
+    mjprizes: ",,,",
+    mjnames: "Mini,Minor,Major,Grand",
+    freeSpinLimit: 0,
+    disableVoiceovers: false,
+  };
+
+  res.json(data);
+  //   res.json({ status: "1" });
+});
+
+app.post("/Playngo/service", (req, res) => {
+  // const obj = {
+  //   data : [`
+  //         d=1 1 10 20 4 3 6 3 1 4 6 4 5 8 1 2 5 0 4 0 0 2 0 2 3 2 6 2 3 3 7
+  //         3 1 0 0 0 1
+  //         6 0
+  //         52 99999600 0 0`],
+  // }
+  //     res.json(obj);
+});
+
+app.listen(4000, () => {
+  console.log("Server is runing on port 4000");
+});
